08bf60eaa43752334c8bc21a913a374f0cdd21c7
Ayzek <Ayzekmanduar@mail.ru>
2022-03-20 01:23:59 +0900

    step 2 90%

--- src/page-loader.js
+++ src/page-loader.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
-// import pageLoader from "./pageLoader.js";
+import pageLoader from "./pageLoader.js";
 
 import { program } from 'commander';
 
+import process from 'process';
 
 program
   .version('0.0.1')
   .arguments('<url>')
   .description('Page loader utility')
-  .option('-o, --output [dir]', 'output dir (default: "/home/user/current-dir")')
+  .option('-o, --output [dir]', `output dir (default: "${process.cwd()}")`)
+  .action((link, path) => {
+    // console.log(link, path.output);
+    pageLoader(link, path.output)
+  })
   .parse();
-// pageLoader('https://ru.hexlet.io/courses');
--- src/pageLoader.js
+++ src/pageLoader.js
@@ -1,18 +1,49 @@
 import axios from 'axios';
 import * as fs from 'fs/promises';
+import process from 'process';
+import * as cheerio from 'cheerio';
+import path from 'path';
+
 
 const parseFileName = (path) => {
   const [, slicePath] = path.split('://');
   const result = slicePath.split('.').join('-').split('/').join('-')
-  return `${result}.html`;
+  const indexHTML = `${result}.html`;
+  const mediaDirName = `${result}_files`;
+  return { indexHTML, mediaDirName };
 };
 
 
-export default async (path) => {
-  axios.get(path).then((res) => {
+export default async (link, pathDir = process.cwd()) => {
+  console.log(link);
+  axios.get(link).then((res) => {
     const { data } = res;
-    const fileName = parseFileName(path)
-    fs.writeFile(`./src/${fileName}`, data, (err) => err);
+    const $ = cheerio.load(data);
+    const images = Array.from($('img'));
+    // console.log(images);
+    const { hostname } = new URL(link);
+    console.log(hostname);
+    // Циклом пройтись и строить пути до картинок
+    const { indexHTML, mediaDirName }= parseFileName(link);
+    fs.writeFile(`${pathDir}/${indexHTML}`, data, (err) => err);
+    fs.mkdir(`${pathDir}/${mediaDirName}`, (err) => err)
+    .then(() => {
+      images.forEach((img) => {
+        const imgSrc = img.attribs.src;
+        axios.get(`https://${hostname}${imgSrc}`, { responseType: 'arraybuffer' })
+        .then(({ data }) => {
+          // console.log(data);
+          // const extname = path.extname(imgSrc);
+          console.log(imgSrc);
+          const normalizedSrc = path.normalize(imgSrc).split('/').join('-');
+          const parsedHostname = hostname.split('.').join('-') 
+          fs.writeFile(`${mediaDirName}/${parsedHostname}${normalizedSrc}`, data, 'base64', (err) => err);  
+        })
+        .catch((error) => console.log(error));
+      })
+    })
   })
   .catch((error) => console.log(error.message));
+  
 };
+// https://en.lyrsense.com/images/userDefault.png
