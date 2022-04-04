0d82ea853e09402c1fbc4b15712e74dda56bad9d
maddclif <maddclif24@gmail.com>
2022-04-04 14:13:24 +0300

    Move functions to module

--- src/pageLoader.js
+++ src/pageLoader.js
@@ -3,87 +3,7 @@ import * as fs from 'fs/promises';
 import process from 'process';
 import * as cheerio from 'cheerio';
 import path from 'path';
-import Listr from 'listr';
-import { addLogger } from 'axios-debug-log';
-import Debug from 'debug';
-
-const debug = Debug('page-loader');
-
-const parseFileName = (path) => {
-  const { origin } = new URL(path);
-  const [, slicePath] = origin.split('://');
-  const result = slicePath.split('.').join('-').split('/').join('-')
-  const indexHTML = `${result}.html`;
-  const mediaDirName = `${result}_files`;
-  return { indexHTML, mediaDirName };
-};
-
-const genFileName = (src, { hostname, mediaDirName }) => {
-  let normalizedSrc;
-  const parsedHostname = hostname.split('.').join('-');
-  if (src.includes('https') || src.includes('http')) {
-    const url = new URL(src);
-    const splitSrc = url.pathname.split('/').join('').split('.').join('-');
-    normalizedSrc = [parsedHostname, splitSrc].join('-');
-    return `${mediaDirName}/${normalizedSrc}`;
-  }
-  normalizedSrc = path.normalize(src).split('/').join('-');
-  return `${mediaDirName}/${parsedHostname}${normalizedSrc}`;
-};
-
-const isUrl = (str) => str.startsWith('http');
-
-const canDownload = (href, hostName) => {
-  if (!href) {
-    return false;
-  }
-  if (isUrl(href)) {
-    const { hostname } = new URL(href);
-    return hostname === hostName;
-  }
-  else if (href.startsWith('//')) {
-    return false;
-  }
-  return true;
-};
-
-const createFile = (fileName, data) => {
-  const encoding = data.name === 'img' ? 'base64' : 'utf-8';
-  fs.writeFile(fileName, data, encoding, (err) => err);
-};
-
-
-const downLoadResourse = (mediaDirName, resourcePath, hostname) => {
-  const url = isUrl(resourcePath) ? resourcePath : `https://${hostname}${resourcePath}`;
-  const fileName = genFileName(resourcePath, { hostname, mediaDirName });
-  const download = axios.create({ baseURL: `https://${hostname}/` });
-
-  addLogger(download, debug);
-  return download(url, { responseType: 'arraybuffer' })
-    .then(({ data }) => createFile(fileName, data));
-};
-
-const loadResources = (html, { hostname, pathDir, mediaDirName, url }) => {
-  const images = Array.from(html('img'));
-  const links = Array.from(html('link'));
-  const scripts = Array.from(html('script')).filter((script) => script.attribs.src);
-  const resources = [...images, ...links, ...scripts];
-
-  const promises = resources.map(({ attribs }) => {
-    const source = attribs.href ? attribs.href : attribs.src;
-    return {
-      title: `${url}${source.slice(1)}`,
-      enabled: () => canDownload(source, hostname),
-      task: () => downLoadResourse(path.join(pathDir, mediaDirName), source, hostname).catch((error) => {
-        const description = error.message ? error.message : error.response.statusText;
-        return Promise.reject(new Error(description));
-      }),
-    }
-  })
-  const tasks = new Listr(promises, { concurrent: true, exitOnError: false });
-  return tasks.run().catch(e => e);
-}
-
+import { parseFileName, loadResources, canLoad, genFileName } from './utils.js';
 
 export default async (url, pathDir = process.cwd()) => {
   const { indexHTML, mediaDirName } = parseFileName(url);
@@ -103,7 +23,7 @@ export default async (url, pathDir = process.cwd()) => {
     html('img, script, link').each(function() {
       const attribute = html(this).attr('href') ? 'href' : 'src';
       const source = html(this).attr(attribute);
-      const validSource = canDownload(source) ? genFileName(source, config) : source;
+      const validSource = canLoad(source) ? genFileName(source, config) : source;
       html(this).attr(attribute, validSource);
     });
     return html;
---
+++ src/utils.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+import * as fs from 'fs/promises';
+import path from 'path';
+import Listr from 'listr';
+
+const parseFileName = (path) => {
+  const { origin } = new URL(path);
+  const [, slicePath] = origin.split('://');
+  const result = slicePath.split('.').join('-').split('/').join('-');
+  const indexHTML = `${result}.html`;
+  const mediaDirName = `${result}_files`;
+  return { indexHTML, mediaDirName };
+};
+
+const genFileName = (src, { hostname, mediaDirName }) => {
+  let normalizedSrc;
+  const parsedHostname = hostname.split('.').join('-');
+  if (src.includes('https') || src.includes('http')) {
+    const url = new URL(src);
+    const splitSrc = url.pathname.split('/').join('').split('.').join('-');
+    normalizedSrc = [parsedHostname, splitSrc].join('-');
+    return `${mediaDirName}/${normalizedSrc}`;
+  }
+  normalizedSrc = path.normalize(src).split('/').join('-');
+  return `${mediaDirName}/${parsedHostname}${normalizedSrc}`;
+};
+
+const isUrl = (str) => str.startsWith('http');
+
+const canLoad = (href, hostName) => {
+  if (!href) {
+    return false;
+  }
+  if (isUrl(href)) {
+    const { hostname } = new URL(href);
+    return hostname === hostName;
+  } else if (href.startsWith('//')) {
+    return false;
+  }
+  return true;
+};
+
+const createFile = (fileName, data) => {
+  const encoding = data.name === 'img' ? 'base64' : 'utf-8';
+  fs.writeFile(fileName, data, encoding, (err) => err);
+};
+
+const downLoadResourse = (mediaDirName, resourcePath, hostname) => {
+  const url = isUrl(resourcePath)
+    ? resourcePath
+    : `https://${hostname}${resourcePath}`;
+  const fileName = genFileName(resourcePath, { hostname, mediaDirName });
+  const download = axios.create({ baseURL: `https://${hostname}/` });
+
+  return download(url, { responseType: 'arraybuffer' }).then(({ data }) =>
+    createFile(fileName, data)
+  );
+};
+
+const loadResources = (html, { hostname, pathDir, mediaDirName, url }) => {
+  const images = Array.from(html('img'));
+  const links = Array.from(html('link'));
+  const scripts = Array.from(html('script')).filter(
+    (script) => script.attribs.src
+  );
+  const resources = [...images, ...links, ...scripts];
+
+  const promises = resources.map(({ attribs }) => {
+    const source = attribs.href ? attribs.href : attribs.src;
+    return {
+      title: `${url}${source.slice(1)}`,
+      enabled: () => canLoad(source, hostname),
+      task: () =>
+        downLoadResourse(
+          path.join(pathDir, mediaDirName),
+          source,
+          hostname
+        ).catch((error) => {
+          const description = error.message
+            ? error.message
+            : error.response.statusText;
+          return Promise.reject(new Error(description));
+        }),
+    };
+  });
+  const tasks = new Listr(promises, { concurrent: true, exitOnError: false });
+  return tasks.run().catch((e) => e);
+};
+
+export {
+    loadResources,
+    canLoad,
+    genFileName,
+    parseFileName,
+};
